b26b929759788f06fadc67d2adf8ffc597171c3c
holykoifish <holykoifish@gmail.com>
2022-04-10 00:27:02 -0500

    added map

--- Train App/ViewController.swift
+++ Train App/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate,    MKMapViewDelegate {
+   
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        mapView.showsUserLocation = true
+        mapView.setUserTrackingMode(MKUserTrackingMode.follow, animated: true)
+
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+    }
+    
     }
 
-
-}
 
